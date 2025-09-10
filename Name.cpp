c49059230bea98798e0db296b1477411fdf240a7
kulakva <kulakva2006@mail.ru>
2025-09-10 22:14:06 +0300

    kaka

---
+++ Name.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int a[1000][1000],b[1000][1000],c[1000][1000];
+
+int main()
+{
+    int a1,a2,a3;
+    cin >> a1 >> a2 >> a3;
+    for(int i=0;i<a1;i++) for(int j=0;j<a2;j++) cin >> a[i][j];
+    for(int i=0;i<a2;i++) for(int j=0;j<a3;j++)  cin >> b[i][j];
+    for(int i=0;i<a1;i++) 
+    {
+        for(int j=0;j<a3;j++)
+        {
+            for(int k = 0;k<a2;k++){
+                c[i][j]+=a[i][k]*b[k][j];
+            }
+        }
+    }
+    for(int i=0;i<a1;i++) 
+    {
+        cout << c[i][0];
+        for(int j=1;j<a3;j++)
+        {
+
+            cout << ' ' << c[i][j];
+        }
+    cout << '\n';
+    }
+}   
