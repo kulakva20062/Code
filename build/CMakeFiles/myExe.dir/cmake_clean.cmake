67c147a4ee10dc5230ad73f843d64b159f192cf6
kulakva <kulakva2006@mail.ru>
2025-09-11 10:07:43 +0300

    kaka2

---
+++ build/CMakeFiles/myExe.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myExe.dir/link.d"
+  "CMakeFiles/myExe.dir/Name.cpp.o"
+  "CMakeFiles/myExe.dir/Name.cpp.o.d"
+  "myExe"
+  "myExe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myExe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
